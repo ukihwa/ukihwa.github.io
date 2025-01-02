971eaab5c8b7a40873662135b651535f0d8630d9
ukihwa <ukihwa@gmail.com>
2025-01-02 17:43:07 +0900

    feat(base): add base header

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,10 +9,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-gray-900 text-gray-50 hover:bg-gray-900/90',
-        destructive: 'bg-red-500 text-gray-50 hover:bg-red-500/90',
-        outline: 'border bg-white hover:bg-gray-100 hover:text-gray-900',
-        secondary: 'bg-gray-100 text-gray-900 hover:bg-gray-100/80',
+        default: 'hover:bg-gray-900/90 bg-gray-900 text-gray-50',
+        destructive: 'bg-red-500 hover:bg-red-500/90 text-gray-50',
+        outline: 'bg-white border hover:bg-gray-100 hover:text-gray-900',
+        secondary: 'hover:bg-gray-100/80 bg-gray-100 text-gray-900',
         ghost: 'hover:bg-selection hover:text-gray-900',
         link: 'text-gray-900 underline-offset-4 hover:underline',
       },
