b0c74e111d959afb5f2f8c602debd106c38e02a5
ukihwa <ukihwa@gmail.com>
2025-01-02 14:58:50 +0900

    chore(config): configure shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ukihwa.github.io',
   base: '/ukihwa.github.io',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
+  },
+  plugins: [tailwindcssAnimate],
+};
